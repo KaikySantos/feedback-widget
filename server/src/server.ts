aeaa35180bafccf611bd31e147d2ddf594ab97e2
kaikySantos <info.kaiky@gmail.com>
2022-05-05 14:54:25 -0300

    feat: folder structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,39 +1,10 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
-import { prisma } from './prisma';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-/*
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "e7ede4d0006b51",
-    pass: "dca625460743d5"
-  }
-});
-*/
-
-app.post('/feedbacks', async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type, 
-      comment,
-      screenshot,
-    }
-  });
-
-  return res.status(201).json({ data: feedback });
-});
-
-app.get('/feedbacks', (req, res) => {
-  return res.send("HELLO!");
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('HTTP Server Runing!');
